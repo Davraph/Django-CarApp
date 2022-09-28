8f5a50e7530dfc43105d511fa238f3e8eaf9ae57
Davraph <rapheal98@icloud.com>
2022-09-28 14:37:07 +0200

    featured Car, Latest Car and Image Gallery

--- Carzone/pages/views.py
+++ Carzone/pages/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    all_cars = Car.objects.order_by('-created_date')
     context = {
-        'teams': teams
+        'teams': teams,
+        'featured_cars': featured_cars,
+        'all_cars': all_cars,
     }
     return render(request, 'pages/home.html', context)
 
 def about(request):
     teams = Team.objects.all()
+    
+
     context = {
-        'teams':teams
+        'teams':teams,
+       
     }
 
     return render(request, 'pages/about.html', context)
